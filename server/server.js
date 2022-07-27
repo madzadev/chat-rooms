26ffbb595076d28dccde6675de377f3979557edc
madzadev <madars.biss@gmail.com>
2022-07-27 07:50:54 +0300

    edit server

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ io.on("connection", (socket) => {
 
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
-    console.log("message sent");
+    // console.log("message sent");
     io.to(user.room).emit("message", { user: user.name, text: message });
     io.to(user.room).emit("roomData", {
       room: user.room,
